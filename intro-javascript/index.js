9e3d9ef10ad03efb07f04b449b202d0983709009
angeloluna <angel.luna.najera@gmail.com>
2024-10-03 20:56:02 -0500

    intro javascript

---
+++ intro-javascript/index.js
@@ -0,0 +1,67 @@
+
+// outputs
+// console.log('hey saludos fca')
+// alert('Esto es un alert')
+
+// let nombre = "angelo"
+let apellido = "luna"
+let numero = 22
+// let res = nombre + numero
+
+// let edad = prompt("Cuál es tu edad?")
+
+// console.log(typeof res)
+
+let x = 30
+// console.log(typeof x)
+x = 'ayer me dijo"hola nada mas"'
+// console.log(typeof x)
+x = x + x
+
+// console.log(typeof x)
+
+
+// "Hola tu nombre es angelo, tienes 34 y tu seria favorita es Arcane"
+
+// let nombre = prompt("Cuál es tu nombre?")
+// let edad = prompt("Cuál es tu edad?")
+// let serie = prompt("Cuál es tu seria favorita?")
+
+// console.log("Hola tu nombre es " + nombre + " tienes " + edad + " y tu serie favorita es " + serie)
+
+// console.log(`Hola tu nombre es ${nombre}, tienes ${edad} y tu seria favorita es ${serie}`)
+
+// console.log(nombre, edad, serie)
+
+// let edad = 18
+// let cedula = true
+
+// if (edad >= 18 ) {
+//   if (cedula == true) {
+//     console.log('Puedes votar')
+//   }
+// } else {
+//   console.log("No puedes votar")
+// }
+
+
+
+let personas = 10
+let pagoTarjeta = false
+let papitas = true
+
+if (personas == 1) {
+  console.log('Mostrtar productos personales')
+  if (papitas) {
+    console.log("mostrar tamaños")
+  }
+} else if(personas > 1 && personas < 4) {
+  console.log('paquete amiguis')
+} else {
+  console.log('familiar')
+}
+
+if (pagoTarjeta) {
+  console.log('Te descontamos')
+}
+
